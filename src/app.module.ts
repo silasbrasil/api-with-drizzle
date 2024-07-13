e4bc15f3902ddae2898ed42e98d4e9069221246a
Silas Brasil <silasbrasil@niduu.com>
2024-07-13 16:38:02 -0300

    feat: init setup of drizzle-orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
   ],
   controllers: [AppController],
 })
